b4db6c4f26a284c5539111a3afc649022db86ea6
NayoLx <690575679@qq.com>
2018-10-25 17:46:12 +0800

    Mywx （V 1.0.8）5 1.U 增加列表页面

--- pages/order/list/index.js
+++ pages/order/list/index.js
@@ -8,7 +8,66 @@ Page({
     loadingHide: '',
     currentTab: 0,
     winWidth: 0,
-    winHeight: 0
+    winHeight: 0,
+    itemData: [
+      {
+        no: '001',
+        img: '/img/ic_need.png',
+        name: '宋双庙',
+        info: '我：二狗，你妈喊你回家吃饭',
+        time: '上午12:00',
+      },
+      {
+        no: '001',
+        img: '/images/mp3.png',
+        name: '订阅号',
+        info: '拜拜吧比较爱白芭比白阿比',
+        time: '昨天',
+      },
+      {
+        no: '001',
+        img: '/images/pdf.png',
+        name: '微信团队',
+        info: '登录操作通知',
+        time: '上午 9:00',
+      },
+      {
+        no: '001',
+        img: '/images/txt.png',
+        name: '锤子科技',
+        info: '罗永浩 x 罗振宇访谈节目 《长谈》',
+        time: '星期二',
+      },
+      {
+        no: '001',
+        img: '/images/word.png',
+        name: '微信公众平台安全助手',
+        info: '小程序登录提醒',
+        time: '上午10:00',
+      },
+      {
+        no: '001',
+        img: '/images/excel.png',
+        name: '微信支付',
+        info: '支付成功',
+        time: '上午12:00',
+      }
+      ,
+      {
+        no: '001',
+        img: '/images/excel.png',
+        name: '微信支付',
+        info: '支付成功',
+        time: '上午12:00',
+      },
+      {
+        no: '001',
+        img: '/images/excel.png',
+        name: '微信支付',
+        info: '支付成功',
+        time: '上午12:00',
+      }
+    ]
   },
 
   bindChange: function (e) {
@@ -47,6 +106,25 @@ Page({
         });
       }
     });
+  },
+
+  touchS: function (e) {  // touchstart
+    let startX = Public.getClientX(e)
+    startX && this.setData({ startX })
+  },
+  touchMove: function (e) {  // touchmove
+    let itemData = Public.touchMove(e, this.data.itemData, this.data.startX)
+    itemData && this.setData({ itemData })
+
+  },
+  touchEnd: function (e) {  // touchend
+    const width = 150  // 定义操作列表宽度
+    let itemData = Public.touchEnd(e, this.data.itemData, this.data.startX, width)
+    itemData && this.setData({ itemData })
+  },
+  itemDelete: function (e) {  // itemDelete
+    let itemData = Public.deleteItem(e, this.data.itemData)
+    itemData && this.setData({ itemData })
   },
 
 })
--- utils/util.js
+++ utils/util.js
@@ -38,8 +38,70 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+
+
+function _getIndex(e) {  // 获取滑动列表的下标值
+    return e.currentTarget.dataset.index
+  }
+
+function _getEndX(e, startX) {  // 获取滑动结束滑动的距离
+    let touch = e.changedTouches
+    if (touch.length === 1) {
+      return touch[0].clientX - startX
+    }
+  }
+
+function _resetData(dataList) {  // 重置数据， 把所有的列表 left 置为 0
+    for (let i in dataList) {
+      dataList[i].left = 0
+    }
+    return dataList
+  }
+
+function getClientX(e) {  // 获取滑动的横坐标
+    let touch = e.touches
+    if (touch.length === 1) {
+      return touch[0].clientX
+    }
+  }
+
+function touchMove(e, dataList, startX) {  // touchmove 过程中更新列表数据
+    let list = this._resetData(dataList)
+    list[this._getIndex(e)].left = -150
+    return list
+  }
+
+function touchEnd(e, dataList, startX, width) {  // touchend 更新列表数据
+    let list = this._resetData(dataList)
+    let disX = this._getEndX(e, startX)
+    let left = 0
+
+    if (disX < 0) {  // 判断滑动方向， （向左滑动）
+      // 滑动的距离大于删除宽度的一半就显示操作列表 否则不显示
+       left = -150 
+    } else {  // 向右滑动复位
+      left = 0
+    }
+
+    list[this._getIndex(e)].left = left
+    return list
+  }
+
+function deleteItem(e, dataList) {  // 删除功能
+    dataList.splice(this._getIndex(e), 1)
+    return dataList
+  }
+
+
 module.exports = {
   formatTime: formatTime,
   showLoading: showLoading,
-  hideLoading: hideLoading
+  hideLoading: hideLoading,
+  _getIndex: _getIndex,
+  _getEndX: _getEndX,
+  _resetData: _resetData,
+  getClientX: getClientX,
+  touchMove: touchMove,
+  touchEnd: touchEnd,
+  deleteItem: deleteItem,
 }
