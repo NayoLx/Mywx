5da8491904184d51dd5518f9328cb205da26f163
NayoLx <690575679@qq.com>
2018-11-20 15:28:56 +0800

    Mywx （V 1.0.8）15 1. U 新增留言页面。并与数据库交互大概实现留言功能

--- pages/order/my/text_detail/detail/index.js
+++ pages/order/my/text_detail/detail/index.js
@@ -1,4 +1,8 @@
 // pages/order/my/text_detail/detail/index.js
+var Public = require('../../../../../utils/pubic.js');
+var toast = require('../../../../../utils/util.js');
+var Da = require("../../../../../utils/fun.js");
+
 Page({
 
   /**
@@ -7,9 +11,14 @@ Page({
   data: {
     textdetail: [],
     focus: false,
+    winHeight: 600,
+    toView: '',
+    detail: '',
+    limit: 10,
   },
 
   getDetail: function() {
+    var that = this
     var data = {
       'postimg': 'http://li.bytodream.cn/images/img/banner_3.jpg',
       'title': '更新目录',
@@ -18,54 +27,108 @@ Page({
       'content': '为了体现产品的人性化，他把对用户的称呼“您”变成“你”，“这个写进我们的产品条约里面去了。后来再也没有人敢在产品中对用户过于尊敬，因为我们一旦对用户过于尊敬，那说明我们可能怀有目的，可能需要骗一点什么东西过来。” 为了保护用户隐私，他坚持微信的系统设计不保留用户的聊天记录，并且坚决不侵犯用户隐私。“我们从来不会给用户发任何的骚扰信息。大家可以回顾一下，在微信里面有没有收到过任何一条系统下发的营销信息，应该是没有的。”',
       'author': 'li',
       'postid': '0',
-      'talk': [{
-          'id': '1540226133',
-          'name': '',
-          'talk': '',
-        'time': '2018-11-14 14:13',
-        },
-        {
-          'id': '1540226133',
-          'name': '',
-          'talk': '',
-          'time': '2018-11-14 14:14',
-        },
-        {
-          'id': '1540226133',
-          'name': '',
-          'talk': '',
-          'time': '2018-11-14 14:15',
-        },
-      ],
       'effect_coupon_num': '10',
     }
+
+    wx.request({
+      url: Da.dataUrl + '?r=comment/getcomment',
+      data: {
+        postid: 0,
+      },
+      method: 'POST',
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+        // 'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        if (res.data.success) {
+          var talk = res.data.detail
+          that.setData({
+            textdetail: data,
+            talk: talk
+          })
+        }
+      }
+    })
+
+
+  },
+
+  onDetail: function(e) {
     this.setData({
-      textdetail: data,
+      detail: e.detail.value,
     })
   },
 
-  changeFocus: function() {
+  changeFocus: function(e) {
     if (this.data.focus == true) {
       this.setData({
         focus: false,
       })
-      return ;
+      return;
     }
     this.setData({
       focus: true,
     })
   },
 
-  click: function () {
+  click: function() {
     if (this.data.click == true) {
       this.setData({
         click: false
       })
-    }
-    else {
+    } else {
       this.setData({
         click: true
       })
+    }
+  },
+
+  closeFocus: function() {
+    this.setData({
+      focus: false,
+    })
+  },
+
+  onJumpComment: function() {
+    this.setData({
+      toView: 'comment'
+    })
+  },
+
+  pushComment: function() {
+    var that = this
+    var openid = wx.getStorageSync('openid')
+    // 显示正在加载...
+    toast.showLoading()
+
+    if (that.data.detail != '') {
+      wx.request({
+        url: Da.dataUrl + '?r=comment/addcomment',
+        data: {
+          openid: openid,
+          comment: that.data.detail,
+          postid: that.data.textdetail.postid,
+        },
+        method: 'POST',
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+          // 'Content-Type': 'application/json'
+        },
+        success: function(res) {
+          if (res.data.status == 'true') {
+            that.setData({
+              value: ''
+            })
+            that.getDetail()
+            // 隐藏加载提示
+            toast.hideLoading()
+          }
+          return;
+        }
+      })
+    } else {
+      Public.show('留言不能为空哦')
     }
   },
 
@@ -73,7 +136,30 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    var that = this
     this.getDetail()
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          winHeight: res.windowHeight
+        })
+      },
+    })
+  },
+
+  //下拉刷新事件
+  scrolltolower: function() {
+    if (this.data.limit < this.data.talk.length) {
+      this.loadMoreDetail()
+      console.log('下拉事件触发')
+    }
+  },
+
+  loadMoreDetail: function() {
+    var limit = this.data.limit
+    this.setData({
+      limit: limit + 10,
+    })
   },
 
   /**
@@ -87,7 +173,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    var setinter = setInterval(this.getDetail, 10000)
   },
 
   /**
@@ -97,5 +183,5 @@ Page({
 
   },
 
-  
+
 })
