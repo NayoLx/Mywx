43e4b920ee8292e9d6adb94a60a5ee4036231eae
NayoLx <690575679@qq.com>
2019-02-28 11:04:05 +0800

    Mywx （V 1.1.0）10 1. U 取消判断订单状态以及接单判断

--- utils/util.js
+++ utils/util.js
@@ -1,7 +1,7 @@
 var Da = require("/fun.js");
 
 /**
- * versionCode: "（V 1.1.0 ）bulid 8" // 版本号
+ * versionCode: "（V 1.1.0 ）bulid 10" // 版本号
  *
  */
 const formatTime = date => {
@@ -110,6 +110,11 @@ function deleteItem(e, dataList) { // 删除功能
     },
     success: function(res) {
       console.log(res.data)
+      if (res.data.success) {
+        that.show('取消成功')
+      } else {
+        that.show(res.data.error);
+      }
     }
   })
   return dataList
